a6223ea7d859c91053e3f51e9e087fe691cfb6f7
jeff <jeff@jamma.cn>
2018-05-06 18:17:07 +0800

    jest

--- tests/service.js
+++ tests/service.js
@@ -1,64 +1,10 @@
-const chai = require('chai')
-let expect = chai.expect
 const config = require('../config')
-const $ = require('../lib/service')
-const consts = require('../lib/consts')
-let Err = consts.Err
+const $ = require('../lib')
 
-let log = function (err, doc) {
-  if (err) console.error(err.stack)
+let service = $(config)
+service.log = (err, doc) => {
+  if (err)
+    console.error(err.stack)
   if (doc) console.log(doc)
 }
-
-let service = new $(config)
-describe('service', function () {
-  it('signon', async function () {
-    let doc = await service.signon({
-      id: 1
-    })
-    expect(doc.token).to.be.ok
-  })
-
-  it('verify', async function () {
-    let doc = await service.signon({
-      id: 1
-    })
-    doc = await service.verify(doc.token)
-    expect(doc.token).to.be.ok
-  })
-
-
-  it('touch', function (done) {
-    service.signon({
-      id: 1,
-      expire: 1,
-    })
-      .then(doc => {
-        return service.touch(doc.token, {expire: 50})
-      })
-      .then(doc => {
-        setTimeout(() => {
-            service.verify(doc.token)
-              .then(doc => {
-                expect(doc.token).to.be.ok
-                done()
-              })
-          },
-          1500)
-      })
-  })
-
-
-  it('signout', async function () {
-    let doc = await service.signon({
-      id: 1
-    })
-    let token = doc.token
-    doc = await service.signout(token)
-    try {
-      doc = await service.verify(token)
-      expect(doc).to.be.not.ok
-    } catch (e) {
-    }
-  })
-})
+module.exports = service
